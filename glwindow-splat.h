c48b948cf7e5daed17055da6a76da7ce139db383
Pier-Antoine Giguère <pagiguere@vigilance.ca>
2023-12-29 15:32:56 -0500

    invert4

--- glwindow-splat.h
+++ glwindow-splat.h
@@ -80,7 +80,43 @@ static std::vector<float> multiply4(std::vector<float> a, std::vector<float> b) 
 	};
 }
 
+static
+std::vector<float> invert4(std::vector<float> a) {
+	float b00 = a[0] * a[5] - a[1] * a[4];
+	float b01 = a[0] * a[6] - a[2] * a[4];
+	float b02 = a[0] * a[7] - a[3] * a[4];
+	float b03 = a[1] * a[6] - a[2] * a[5];
+	float b04 = a[1] * a[7] - a[3] * a[5];
+	float b05 = a[2] * a[7] - a[3] * a[6];
+	float b06 = a[8] * a[13] - a[9] * a[12];
+	float b07 = a[8] * a[14] - a[10] * a[12];
+	float b08 = a[8] * a[15] - a[11] * a[12];
+	float b09 = a[9] * a[14] - a[10] * a[13];
+	float b10 = a[9] * a[15] - a[11] * a[13];
+	float b11 = a[10] * a[15] - a[11] * a[14];
+	float det =
+		b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+	if (det < 0.0000001) return {};
 
+	return{
+			(a[5] * b11 - a[6] * b10 + a[7] * b09) / det,
+			(a[2] * b10 - a[1] * b11 - a[3] * b09) / det,
+			(a[13] * b05 - a[14] * b04 + a[15] * b03) / det,
+			(a[10] * b04 - a[9] * b05 - a[11] * b03) / det,
+			(a[6] * b08 - a[4] * b11 - a[7] * b07) / det,
+			(a[0] * b11 - a[2] * b08 + a[3] * b07) / det,
+			(a[14] * b02 - a[12] * b05 - a[15] * b01) / det,
+			(a[8] * b05 - a[10] * b02 + a[11] * b01) / det,
+			(a[4] * b10 - a[5] * b08 + a[7] * b06) / det,
+			(a[1] * b08 - a[0] * b10 - a[3] * b06) / det,
+			(a[12] * b04 - a[13] * b02 + a[15] * b00) / det,
+			(a[9] * b02 - a[8] * b04 - a[11] * b00) / det,
+			(a[5] * b07 - a[4] * b09 - a[6] * b06) / det,
+			(a[0] * b09 - a[1] * b07 + a[2] * b06) / det,
+			(a[13] * b01 - a[12] * b03 - a[14] * b00) / det,
+			(a[8] * b03 - a[9] * b01 + a[10] * b00) / det
+	};
+}
 
 
 class GLWindowSplat : public QOpenGLWindow {
